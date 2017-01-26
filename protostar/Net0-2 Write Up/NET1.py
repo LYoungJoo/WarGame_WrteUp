80c616db450022ec53b9c7e5946ec53d98d7a0ec
LeeYoungJoo <boright77@naver.com>
2017-01-26 17:14:30 +0900

    Create NET1.py

---
+++ protostar/Net0-2 Write Up/NET1.py
@@ -0,0 +1,12 @@
+# using pwntool : https://github.com/Gallopsled/pwntools
+from pwn import *
+
+s = remote('10.211.55.9', 2998)
+
+data = u32(s.recv(1024))
+
+print "[+] RECV " + str(data)
+print "[+] SEND "
+s.sendline(str(data))
+
+print "[+] " + s.recvline()
